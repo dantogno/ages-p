df0a628d181dee0c1b4c3e87e359aee1658c004d
Tim <timothy.c13@gmail.com>
2019-04-04 14:32:42 -0500

    Prefabs complete
    +Fragment Prefabs
    +Interactive script updated

--- Assets/Script/InteractiveObject.cs
+++ Assets/Script/InteractiveObject.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class InteractiveObject : MonoBehaviour, IInteractive
 {
+    [SerializeField] private string displayText = nameof(InteractiveObject);
+    private AudioSource audioSource;
+
+    public string DisplayText => displayText;
+    
+     private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void InteractWith()
     {
+        try
+        {
+        audioSource.Play();
+        }
+        catch (System.Exception)
+        {
+
+            throw new System.Exception("Missing audiosource component requires an audio Source component.");
+        }
+    
         Debug.Log($"Player just interaacted with {gameObject.name}.");
     }
+
+   
 }
